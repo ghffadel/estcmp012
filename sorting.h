158bf3e49d9077ebd547db4e066f6d3de5b16f8d
Gustavo Fadel <ghff.snf20@uea.edu.br>
2022-02-01 11:26:48 -0400

    Added 2 sorting algorithms

--- sorting.h
+++ sorting.h
@@ -41,3 +41,38 @@ void insertion_sort (int vet[], int n) {
     vet[j + 1] = key;
   }
 }
+
+void shell_sort (int vet[], int n) {
+  int gaps[] = {701, 301, 132, 57, 23, 10, 4, 1};
+
+  for (int h = 0; h < 8; h++) {
+    for (int i = gaps[h]; i < n; i++) {
+      int j, key = vet[i];
+
+      for (j = i - gaps[h]; j >= 0 && vet[j] > key; j -= gaps[h]) {
+        vet[j + gaps[h]] = vet[j];
+      }
+
+      vet[j + gaps[h]] = key;
+    }
+  }
+}
+
+int partition (int vet[], int left, int right) {
+  int i = left - 1, j = right + 1, pivot = vet[(left + right) / 2];
+
+  while (1) {
+    for (; vet[i] < pivot; i++);
+    for (; vet[j] > pivot; j--);
+    if (i >= j) return j;
+    swap(&vet[i], &vet[j]);
+  }
+}
+
+void quick_sort (int vet[], int left, int right) {
+  if (left < right) {
+    int p = partition(vet, left, right);
+    quick_sort(vet, left, p);
+    quick_sort(vet, p + 1, right);
+  }
+}
